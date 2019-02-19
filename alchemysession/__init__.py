7e552382a29fa1a4bd42d0a75b24c4cb8bc383ac
Tulir Asokan <tulir@maunium.net>
2019-02-20 01:44:58 +0200

    Bump version to 0.2.9

--- alchemysession/__init__.py
+++ alchemysession/__init__.py
@@ -1,4 +1,4 @@
 from .sqlalchemy import AlchemySessionContainer
 
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 __author__ = "Tulir Asokan <tulir@maunium.net>"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="telethon-session-sqlalchemy",
-    version="0.2.8",
+    version="0.2.9",
     url="https://github.com/tulir/telethon-session-sqlalchemy",
 
     author="Tulir Asokan",
