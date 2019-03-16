7a96f29712e6c4f863cb5ab16bb1bfec62ca1c4c
Tulir Asokan <tulir@maunium.net>
2019-03-16 16:59:51 +0200

    Allow using core mode without passing an ORM session

--- alchemysession/__init__.py
+++ alchemysession/__init__.py
@@ -1,4 +1,4 @@
 from .sqlalchemy import AlchemySessionContainer
 
-__version__ = "0.2.10"
+__version__ = "0.2.11"
 __author__ = "Tulir Asokan <tulir@maunium.net>"
--- alchemysession/sqlalchemy.py
+++ alchemysession/sqlalchemy.py
@@ -1,7 +1,8 @@
-from typing import Optional, Tuple, Any
+from typing import Optional, Tuple, Any, Union
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, BigInteger, LargeBinary, orm
+from sqlalchemy.orm.scoping import scoped_session
+from sqlalchemy import Column, String, Integer, BigInteger, LargeBinary, orm, func, select
 import sqlalchemy as sql
 
 from .orm import AlchemySession
@@ -14,15 +15,19 @@ LATEST_VERSION = 2
 
 
 class AlchemySessionContainer:
-    def __init__(self, engine=None, session: orm.Session = None, table_prefix: str = "",
-                 table_base: Optional[declarative_base] = None, manage_tables: bool = True) -> None:
+    def __init__(self, engine: Union[sql.engine.Engine, str] = None,
+                 session: Optional[Union[orm.Session, scoped_session, bool]] = None,
+                 table_prefix: str = "", table_base: Optional[declarative_base] = None,
+                 manage_tables: bool = True) -> None:
         if isinstance(engine, str):
             engine = sql.create_engine(engine)
 
         self.db_engine = engine
-        if not session:
+        if session is None:
             db_factory = orm.sessionmaker(bind=self.db_engine)
             self.db = orm.scoping.scoped_session(db_factory)
+        elif not session:
+            self.db = None
         else:
             self.db = session
 
@@ -31,8 +36,13 @@ class AlchemySessionContainer:
          self.SentFile, self.UpdateState) = self.create_table_classes(self.db, table_prefix,
                                                                       table_base)
         self.alchemy_session_class = AlchemySession
+        if not self.db:
+            # Implicit core mode if there's no ORM session.
+            self.core_mode = True
 
         if manage_tables:
+            if not self.db:
+                raise ValueError("Can't manage tables without an ORM session.")
             table_base.metadata.bind = self.db_engine
             if not self.db_engine.dialect.has_table(self.db_engine,
                                                     self.Version.__tablename__):
@@ -58,13 +68,17 @@ class AlchemySessionContainer:
             else:
                 self.alchemy_session_class = AlchemyCoreSession
         else:
+            if not self.db:
+                raise ValueError("Can't use ORM mode without an ORM session.")
             self.alchemy_session_class = AlchemySession
 
     @staticmethod
-    def create_table_classes(db, prefix: str, base: declarative_base
+    def create_table_classes(db: scoped_session, prefix: str, base: declarative_base
                              ) -> Tuple[Any, Any, Any, Any, Any]:
+        qp = db.query_property() if db else None
+
         class Version(base):
-            query = db.query_property()
+            query = qp
             __tablename__ = "{prefix}version".format(prefix=prefix)
             version = Column(Integer, primary_key=True)
 
@@ -72,7 +86,7 @@ class AlchemySessionContainer:
                 return "Version('{}')".format(self.version)
 
         class Session(base):
-            query = db.query_property()
+            query = qp
             __tablename__ = '{prefix}sessions'.format(prefix=prefix)
 
             session_id = Column(String(255), primary_key=True)
@@ -87,7 +101,7 @@ class AlchemySessionContainer:
                                                                 self.auth_key)
 
         class Entity(base):
-            query = db.query_property()
+            query = qp
             __tablename__ = '{prefix}entities'.format(prefix=prefix)
 
             session_id = Column(String(255), primary_key=True)
@@ -103,7 +117,7 @@ class AlchemySessionContainer:
                                                                        self.phone, self.name)
 
         class SentFile(base):
-            query = db.query_property()
+            query = qp
             __tablename__ = '{prefix}sent_files'.format(prefix=prefix)
 
             session_id = Column(String(255), primary_key=True)
@@ -119,7 +133,7 @@ class AlchemySessionContainer:
                                                                    self.type, self.id, self.hash)
 
         class UpdateState(base):
-            query = db.query_property()
+            query = qp
             __tablename__ = "{prefix}update_state".format(prefix=prefix)
 
             session_id = Column(String(255), primary_key=True)
@@ -158,8 +172,22 @@ class AlchemySessionContainer:
     def new_session(self, session_id: str) -> 'AlchemySession':
         return self.alchemy_session_class(self, session_id)
 
+    def has_session(self, session_id: str) -> bool:
+        if self.core_mode:
+            t = self.Session.__table__
+            rows = self.db_engine.execute(select([func.count(t.c.auth_key)])
+                                          .where(t.c.session_id == session_id))
+            try:
+                count, = next(rows)
+                return count > 0
+            except StopIteration:
+                return False
+        else:
+            return self.Session.query.filter(self.Session.session_id == session_id).count() > 0
+
     def list_sessions(self):
         return
 
     def save(self) -> None:
-        self.db.commit()
+        if self.db:
+            self.db.commit()
